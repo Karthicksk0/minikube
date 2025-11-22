f28c3132eb29da21fed7f267ebc1ed9bdd1d1acb
Karthicksk0 <karthicksivakumar00@gmail.com>
2025-11-22 19:00:04 +0000

    Fix CI test: prevent Flask server from running in Actions

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Kubernetes on Minikube to karthick!"
+    return "Hello from CI/CD on Minikube!"
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
 
